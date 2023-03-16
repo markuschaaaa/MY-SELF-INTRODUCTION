e00740f6d8d347cbaebb9e98b5b6e70a9956c5ad
markuschaaaa <chumakoff03@gmail.com>
2023-03-16 21:06:16 +0300

    feat: fill skills-section with info

--- src/pages/skills/index.ts
+++ src/pages/skills/index.ts
@@ -12,18 +12,34 @@ class SkillsPage extends Page {
    addSkills(){
       const skillsWrapper = document.createElement("div");
       const skillsContainer = document.createElement("div");
+      const skillsItemWrapper = document.createElement("div");
       const skillsItemOne = document.createElement("div");
       const skillsItemTwo = document.createElement("div");
+      const skillsItemThree = document.createElement("div");
+
+      const images = ['css.png', 'html.png', 'js.png', 'git.png', 'gitt.png', 'node.png', 'sass.png', 'ts.png']
 
       skillsWrapper.classList.add('skills__wrapper');
       skillsContainer.classList.add('skills__container');
+      skillsItemWrapper.classList.add('skillsitem__wrapper');
       skillsItemOne.classList.add('skills__item');
       skillsItemTwo.classList.add('skills__item');
+      skillsItemThree.classList.add('skills__stack');
 
-      skillsItemOne.innerHTML = "My Softskills:"
-      skillsItemTwo.innerHTML = "My Hardskills:"
+      skillsItemThree.innerText = 'My stack is:'
+      images.forEach((e)=>{
+         const img = document.createElement('img');
+         img.classList.add('skills__img')
+         img.src = `/src/assets/logos/` + e;
 
-      skillsContainer.append(skillsItemOne, skillsItemTwo)
+         skillsItemThree.append(img)
+      })
+
+      skillsItemOne.innerHTML = `My Softskills:\n<ul class="skills__list"><li>Teamwork</li><li>Reliability and result orientation</li><li>Determined and conscientious</li><li>Self-Motivation and Independence</li><li>Communication</li></ul>`
+      skillsItemTwo.innerHTML = `My Hardskills:\n<ul class="skills__list"><li>Object-oriented programming (OOP) languages.</li><li>BEM-block element modifier methodology</li><li>Git version control</li></ul>`;
+
+      skillsItemWrapper.append(skillsItemOne,skillsItemTwo)
+      skillsContainer.append(skillsItemWrapper,skillsItemThree)
       skillsWrapper.appendChild(skillsContainer);
       this.container.append(skillsWrapper);
    }
